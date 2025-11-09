d02051da3bf8b8d465b85e00a233e634fcf67548
Mintu Sikder <mintusikder15@gmail.com>
2025-11-09 11:30:29 +0600

    object type and literal types

--- src/nonPrimitive.ts
+++ src/nonPrimitive.ts
@@ -1,4 +1,4 @@
-// array , object 
+// array , object
 let bajarlist: string[] = ["elma", "armut", "muz"];
 
 bajarlist.push(12);
@@ -7,4 +7,40 @@ let mixArray: (string | number | boolean)[] = [1, "iki", true];
 
 mixArray.push(false);
 
-let con : [number,number] = [20,20,5]
+let con: [number, number] = [20, 20, 5];
+
+//reference type : object
+
+// const user: {
+//   organization: "Tech Corp";  //value => type : literal types
+//   firstName: string;
+//   middleName?: string;
+//   lastName: string;
+//   isMarred: boolean;
+// } = {
+//   organization: "Tech Corp",
+//   firstName: "John",
+//   middleName: "something",
+//   lastName: "Doe",
+//   isMarred: false,
+// };
+
+// user.organization = "New Org";
+
+const user: {
+  readonly organization: string; // access modifier
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  isMarred: boolean;
+} = {
+  organization: "Tech Corp",
+  firstName: "John",
+  middleName: "something",
+  lastName: "Doe",
+  isMarred: false,
+};
+
+user.organization = "New Org";
+
+console.log(user);
