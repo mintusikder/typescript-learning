d511dcc53656cc824f3607ec3ae0830c670a0663
Mintu Sikder <mintusikder15@gmail.com>
2025-11-11 22:57:25 +0600

    added gnerics with interface

---
+++ src/gnericsWithInterface.ts
@@ -0,0 +1,55 @@
+interface developer<T,X> {
+  name: string;
+  salary: number;
+  device: {
+    brand: string;
+    model: string;
+    publishDate: string;
+  };
+  smartWatch: T;
+  bike?: X
+}
+
+interface smartWatch {
+  heartRate: string;
+  stopWatch?: boolean;
+  callSupport?: boolean;
+  ai?: boolean;
+}
+
+const poorDeveloper: developer<smartWatch> = {
+  name: "John Doe",
+  salary: 50000,
+  device: {
+    brand: "Dell",
+    model: "XPS 13",
+    publishDate: "2020-01-15",
+  },
+  smartWatch: {
+    heartRate: "72 bpm",
+    stopWatch: true,
+  },
+};
+
+interface advancedSmartWatch {
+  heartRate: string;
+  stopWatch: boolean;
+  callSupport: boolean;
+  ai: boolean;
+  gps: boolean;
+}
+
+const richDeveloper: developer<advancedSmartWatch> = {
+  name: "Mintu Sikder",
+  salary: 90000,
+  device: {
+    brand: "Hp",
+    model: "XPS 13",
+    publishDate: "2020-01-15",
+  },
+  smartWatch: {
+    heartRate: "72 bpm",
+    callSupport: true,
+    ai: true,
+  },
+};
